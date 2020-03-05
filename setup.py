7d609af8f7aab659dcb146a73cace288ee42ca7e
microprediction <info@3za.org>
2020-03-04 20:05:15 -0500

    version s Please enter the commit message for your changes. Lines starting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="muid",
-    version="0.0.4",
+    version="0.0.5",
     description="Memorable Unique Identifier",
     long_description=README,
     long_description_content_type="text/markdown",
