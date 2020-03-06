263cb5838ede2ccd64f6be75bd1a65d2392e34ad
microprediction <info@3za.org>
2020-03-06 13:46:23 -0500

    version 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="muid",
-    version="0.0.8",
+    version="0.1.0",
     description="Memorable Unique Identifier",
     long_description=README,
     long_description_content_type="text/markdown",
