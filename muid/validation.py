6dfeee810a723ba0681b063240ef81b243921e9d
microprediction <info@3za.org>
2020-03-14 22:41:09 -0400

    Now converts from str to bytes when calling animal

--- muid/validation.py
+++ muid/validation.py
@@ -2,7 +2,8 @@ from muid.corpus import search
 from muid.crypto import bhash
 
 def animal(key):
-    code = bhash(key)
+    bkey = key if isinstance(key,bytes) else key.encode('ascii')
+    code = bhash(bkey)
     return search(code)
 
 def validate(key):
