f29c89f32ae0f7e59931c1e39a8700b8ab1f3f84
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-09-28 23:52:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="muid",
-    version="0.5.3",
+    version="0.6.0",
     description="Memorable Unique Identifier",
     long_description=README,
     long_description_content_type="text/markdown",
